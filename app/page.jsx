4fafc75835cc87b78f71c43b0adeed84eb991853
Serhii_Mahas <serhii.mahas@evlue.cz>
2024-07-14 17:41:29 +0200

    Created Layout and Home pages

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const metadata = {
+  title: "PropertyPulse | Home App Page",
+  description: "Home App Page",
+  keywords: "rental",
+};
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
